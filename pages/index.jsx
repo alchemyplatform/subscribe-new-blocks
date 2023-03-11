566b5cbded7c691792a666b94609b6bf77fe0e64
elanh <elan@alchemyapi.io>
2023-03-10 17:16:03 -0800

    removed instructions component

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div>
       <main className={styles.main}>
         <BlocksComponent />
-        {/* <InstructionsComponent></InstructionsComponent> */}
       </main>
     </div>
   );
